944dd44e462f316cd61623d5f8f69f2aa67cb87a
Juan-SebastianOA <sebasalvarez622@gmail.com>
2024-04-18 18:50:54 -0500

    último commit

--- src/main/java/co/edu/uniquindio/proyecto/controladores/ServiciosPublicosControlador.java
+++ src/main/java/co/edu/uniquindio/proyecto/controladores/ServiciosPublicosControlador.java
@@ -22,8 +22,8 @@ public class ServiciosPublicosControlador {
     private final ComentarioServicio comentarioServicio;
     private final NegocioServicio negocioServicio;
 
-    @GetMapping("/listar-comentarios-negocio")
-    public ResponseEntity<MensajeDTO<List<ItemComentarioDTO>>> listarComentariosNegocio(String codigo) throws Exception {
+    @GetMapping("/listar-comentarios-negocio/{codigo}")
+    public ResponseEntity<MensajeDTO<List<ItemComentarioDTO>>> listarComentariosNegocio(@PathVariable String codigo)throws Exception {
         return ResponseEntity.ok().body( new MensajeDTO<>(false, comentarioServicio.listarComentariosNegocio(codigo) )
         );
     }
--- src/test/resources/dataset.js
+++ src/test/resources/dataset.js
@@ -2,6 +2,16 @@ db = connect( 'mongodb://root:example@localhost:27017/proyecto?authSource=admin'
 
 db.Moderadores.insertMany([
     {
+        _id: "Moderador1",
+        email: "moderador1@email.com",
+        password : "moderador1"
+    },
+    {
+        _id: "Moderador2",
+        email: "moderador2@email.com",
+        password : "moderador2"
+    },
+    {
         _id: "Moderador3",
         email: "moderador3@email.com",
         password : "moderador3"
